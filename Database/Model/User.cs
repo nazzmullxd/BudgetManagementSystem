8d4511cb23e727cb6814a20cb153c3a93960439b
Nazmul <mdnazmul723048@gmail.com>
2024-11-30 20:21:54 +0600

    Database:Model User Class Properties#

---
+++ Database/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.Model
+{
+    public class User
+    {
+        [Key]
+        public string UserId { get; set; }=Guid.NewGuid().ToString();//To Genetate Default value when value not taken 
+        [Required]
+        public string? Name { get; set; }//Takes Null value given by User ?
+        [Required,EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? PasswordHash { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
