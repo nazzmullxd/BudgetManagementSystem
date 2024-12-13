63c4dc72f8e41ffefc865adb21e9ea56a6396b25
ShiShiR <k.i.d.s69@outlook.com>
2024-12-13 17:52:56 +0600

    DB Context

---
+++ Database/Context/DefaultContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Context
+{
+    public class DefaultContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True;ConnectRetryCount=0");
+        }
+    }
+}
