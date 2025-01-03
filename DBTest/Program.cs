c7e6271dbe88cca0b6186ba51e754b428f122614
Nazmul <mdnazmul723048@gmail.com>
2025-01-03 22:09:35 +0600

    DB Updated

---
+++ DBTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using Database.Model;
+
+namespace Database.Context
+{
+    internal class DBTest
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var db = new BudgetManagementContext())
+                {
+                    Console.WriteLine("Database connection initialized successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error initializing database: {ex.Message}");
+            }
+        }
+    }
+
+    class DBInsertTest
+    {
+        public DBInsertTest()
+        {
+            try
+            {
+                using (var db = new BudgetManagementContext())
+                {
+                    Console.WriteLine("Starting database operation at: " + DateTime.Now);
+
+                    User myUser = new User
+                    {
+                        Name = "Test User",
+                        Email = "admin@admin.admin",
+                        PasswordHash = "Hash",
+                        IsActive = true
+                    };
+
+                    db.Users.Add(myUser);
+                    int rowsAffected = db.SaveChanges();
+
+                    Console.WriteLine(rowsAffected > 0 ? "Data saved successfully." : "No rows affected.");
+                    Console.WriteLine("Finished database operation at: " + DateTime.Now);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Database/Context/BudgetManagementContext.cs
@@ -0,0 +1,20 @@
+﻿using Database.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Context
+{
+    public class BudgetManagementContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=NASIM\MSSQLSERVER01;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TrackIncome> TrackIncomes { get; set; }
+        public DbSet<TrackExpense> TrackExpenses { get; set; }
+        public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+        public DbSet<DuesManagement> DuesManagements { get; set; }
+        public DbSet<BudgetAlerts> BudgetAlerts { get; set; }
+    }
+}
